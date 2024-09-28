5563fc06a639f7ced43ef5c1da7e47f7153020b2
akash <akashpatelsingh9893693091@gmail.com>
2024-09-29 00:18:28 +0530

    "."

--- src/app/config/dbConnect.ts
+++ src/app/config/dbConnect.ts
@@ -1,23 +1,23 @@
 import mongoose from "mongoose";
 
-type ConnectionObject = {
-  isConnected?: number;
-};
+// type ConnectionObject = {
+//   isConnected?: number;
+// };
 
-const connection: ConnectionObject = {};
+// const connection: ConnectionObject = {};
 
 async function dbConnect() {
-  if (connection.isConnected) {
-    console.log("Already Connected to the Database");
-    return;
-  }
+  // if (connection.isConnected) {
+  //   console.log("Already Connected to the Database");
+  //   return;
+  // }
   try {
     const db = await mongoose.connect("mongodb+srv://akashpatelsingh9893693091:NDlTj64F5e8ShkXp@cluster0.lprakem.mongodb.net/movieTestDB?retryWrites=true&w=majority&appName=Cluster0");
-    connection.isConnected = db.connections[0].readyState;
+    // connection.isConnected = db.connections[0].readyState;
     console.log("Database Sucessfully Connected");
   } catch (error) {
     console.log(error);
-    process.exit(1);
+    // process.exit(1);
   }
 }
 
