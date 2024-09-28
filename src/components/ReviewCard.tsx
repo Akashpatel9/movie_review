8785667bf681b0ef65edfde04704b18d43a0fad9
akash <akashpatelsingh9893693091@gmail.com>
2024-09-28 23:34:48 +0530

    "."

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import axios from "axios";
-import React, { useEffect } from "react";
+import React from "react";
 import { AiFillEdit } from "react-icons/ai";
 import { MdDelete } from "react-icons/md";
 
